8f04666d67dfec667c11cbd372be09ad3592202a
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2025-09-21 13:17:55 +0600

    add product managing functionality through api

---
+++ backend/src/main/java/org/istiaqfuad/learn/repo/ProductRepo.java
@@ -0,0 +1,9 @@
+package org.istiaqfuad.learn.repo;
+
+import org.istiaqfuad.learn.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
